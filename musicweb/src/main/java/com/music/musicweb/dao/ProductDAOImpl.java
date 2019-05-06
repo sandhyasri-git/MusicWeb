211d164543fa47f58b71129769d6dc11358c564d
sandhyasri-git <sandhyasri-git@users.noreply.github.com>
2019-05-06 10:35:47 +0530

    Update ProductDAOImpl.java

--- musicweb/src/main/java/com/music/musicweb/dao/ProductDAOImpl.java
+++ musicweb/src/main/java/com/music/musicweb/dao/ProductDAOImpl.java
@@ -37,6 +37,7 @@ public class ProductDAOImpl implements ProductDAO {
 		{
 		Session session=sessionFactory.getCurrentSession();
 		session.update(product);
+			System.out.println("Product updated");
 		return true;
 		}
 		catch(Exception e)
